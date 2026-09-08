352acc87efbd0420abf30d8b8899fec529a2ab58
João Victor Feliciano Pereira <jv.fpereira10@gmail.com>
2026-09-08 15:07:11 -0300

    func: remover itens

--- scripts.js
+++ scripts.js
@@ -7,17 +7,24 @@ form.addEventListener("submit", function(event){
 
     let itemDigitado = inputValue.value.trim()
 
+        if (itemDigitado == ""){
+            return
+        }
+
     const li = document.createElement("li")
-    li.classList.add("itens-li")
+        li.classList.add("itens-li")
 
     const label = document.createElement("label")
+    
     const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
+        checkbox.type = "checkbox"
+    
     const span = document.createElement("span")
-    span.textContent = itemDigitado
+        span.textContent = itemDigitado
+    
     const removeButton = document.createElement("button")
-    removeButton.classList.add("btn-remover")
-    removeButton.textContent = "🗑"
+        removeButton.classList.add("btn-remover")
+        removeButton.textContent = "🗑"
 
     label.appendChild(checkbox)
     label.appendChild(span)
@@ -27,8 +34,17 @@ form.addEventListener("submit", function(event){
 
     lista.appendChild(li)
 
-    console.log(itemDigitado)
+    inputValue.value = ""
 })
+
+lista.addEventListener("click", function(event) {
+    if (event.target.classList.contains("btn-remover")) {
+        const li = event.target.closest("li")
+        li.remove()
+    }
+})
+
+
 
 inputValue.addEventListener("input", function() {
 })
